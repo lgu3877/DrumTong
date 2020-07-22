45d78ca2fadb4049a6606acfb6876aa4d61c4852
Lee gun uck <lgu3877@naver.com>
2020-07-22 09:51:51 +0900

    이건욱 : CPrivateDataVO commit 20200722 09:51분 master merge

---
+++ src/main/java/com/drumtong/customer/vo/CPrivateDataVO.java
@@ -0,0 +1,71 @@
+package com.drumtong.customer.vo;
+
+public class CPrivateDataVO {
+	private String memberid;
+	private String id;
+	private String pw;
+	private String name;
+	private String birth;
+	private String genderboolean;
+	private String phonenum;
+	private String address;
+	private String email;
+	
+	public String getMemberid() {
+		return memberid;
+	}
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+	public String getId() {
+		return id; 
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getGenderboolean() {
+		return genderboolean;
+	}
+	public void setGenderboolean(String genderboolean) {
+		this.genderboolean = genderboolean;
+	}
+	public String getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
