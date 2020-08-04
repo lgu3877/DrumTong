383e89aafcf12d785278d8ff03e1c36d0c948917
YoungKyeongLee <68047956+YoungKyeongLee@users.noreply.github.com>
2020-08-04 15:32:43 +0900

    test

--- src/main/java/com/drumtong/customer/service/main/CustomerMainService.java
+++ src/main/java/com/drumtong/customer/service/main/CustomerMainService.java
@@ -1,21 +1,16 @@
 package com.drumtong.customer.service.main;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.drumtong.customer.dao.CustomerDAO;
 
 @Service
 public class CustomerMainService {
-	@Autowired CustomerDAO dao;
+//	@Autowired CustomerDAO dao;
 	
 	public ModelAndView test() {
-		ModelAndView mv = new ModelAndView("customer/main/customerMain");
-		mv.addObject("test",dao.test());
-		return mv;
+		return new ModelAndView("customer/main/customerMain");
 	}
-	
 	
 	//@Transactional(timeout=5)
 }
