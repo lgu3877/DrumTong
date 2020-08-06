e80156d8f887018055c37c18d7fe6ae4741c08e2
YoungKyeongLee <68047956+YoungKyeongLee@users.noreply.github.com>
2020-08-06 09:13:34 +0900

    이영경 : Business 관련 VO 20200806 09:13 commit

---
+++ src/main/java/com/drumtong/business/vo/BMenuVO.java
@@ -0,0 +1,48 @@
+package com.drumtong.business.vo;
+
+public class BMenuVO {
+	private String estid;
+	private int num;
+	private String name;
+	private int price;
+	private char quickprice;
+	private int ete;
+	public String getEstid() {
+		return estid;
+	}
+	public void setEstid(String estid) {
+		this.estid = estid;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public char getQuickprice() {
+		return quickprice;
+	}
+	public void setQuickprice(char quickprice) {
+		this.quickprice = quickprice;
+	}
+	public int getEte() {
+		return ete;
+	}
+	public void setEte(int ete) {
+		this.ete = ete;
+	}
+	
+	
+}
