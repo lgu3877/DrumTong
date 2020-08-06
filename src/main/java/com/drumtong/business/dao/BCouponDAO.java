675d0460b1a35a8ea0facaca6878c46f990691c9
YoungKyeongLee <68047956+YoungKyeongLee@users.noreply.github.com>
2020-08-06 14:19:46 +0900

    이영경 : customer detail 페이지에서 사용할 DB 세팅 완료 20200806 02:19 commit

--- src/main/java/com/drumtong/business/dao/BCouponDAO.java
+++ src/main/java/com/drumtong/business/dao/BCouponDAO.java
@@ -1,5 +1,9 @@
 package com.drumtong.business.dao;
 
+import com.drumtong.business.vo.BCouponVO;
+
 public interface BCouponDAO {
+
+	BCouponVO select(String estid);
 
 }
--- src/main/java/com/drumtong/business/dao/BManagementDAO.java
+++ src/main/java/com/drumtong/business/dao/BManagementDAO.java
@@ -1,5 +1,9 @@
 package com.drumtong.business.dao;
 
+import com.drumtong.business.vo.BManagementVO;
+
 public interface BManagementDAO {
+
+	BManagementVO selectCustomerDetail(String estid);
 
 }
