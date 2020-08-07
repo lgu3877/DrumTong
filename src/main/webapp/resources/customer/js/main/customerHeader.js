453bbe0611e930c9fea8ccb354e5a01ada59e460
Lee gun uck <lgu3877@naver.com>
2020-08-07 10:54:56 +0900

    etest

--- src/main/webapp/resources/customer/js/main/customerHeader.js
+++ src/main/webapp/resources/customer/js/main/customerHeader.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 		$('.laundryLists').slick({
 		infinite : true, 				// 무한 반복 옵션
-		slidesToShow : 5, 				// 한 화면에 보여질 컨텐츠 개수
+		slidesToShow : 3, 				// 한 화면에 보여질 컨텐츠 개수
 		slidesToScroll : 1, 			// 스크롤 한번에 움직일 컨텐츠 개수
 		autoplay : true, 				// 자동 스크롤 사용 여부
 		autoplaySpeed : 3000, 			// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
