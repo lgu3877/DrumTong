c1c7f22d2521a7806becefe5e08b7ab0cbd24a74
Lee gun uck <lgu3877@naver.com>
2020-08-10 12:16:50 +0900

    이건욱 : search commit 20200810 12:16분 master merge

--- src/main/java/com/drumtong/customer/service/laundry/CustomerLaundryService.java
+++ src/main/java/com/drumtong/customer/service/laundry/CustomerLaundryService.java
@@ -1,8 +1,15 @@
 package com.drumtong.customer.service.laundry;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 @Service
 public class CustomerLaundryService {
+	
+	// 검색 페이지 이동 [GET]
+	public ModelAndView search() {
+		ModelAndView mav = new ModelAndView("customer/laundry/customerSearch");
+		return mav;
+	}
 
 }
