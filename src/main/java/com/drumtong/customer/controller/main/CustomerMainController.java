27e820d91dc13e3551302dcfff434a948a20e54b
YoungKyeongLee <68047956+YoungKyeongLee@users.noreply.github.com>
2020-08-13 11:29:45 +0900

    이영경 : 충돌 파일 commit 20200813 11:29 commit

--- src/main/java/com/drumtong/customer/controller/main/CustomerMainController.java
+++ src/main/java/com/drumtong/customer/controller/main/CustomerMainController.java
@@ -1,9 +1,13 @@
 package com.drumtong.customer.controller.main;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.drumtong.customer.service.main.CustomerMainService;
 
 @Controller
 @RequestMapping(value = {"/customer/", "/"})
