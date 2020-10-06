61c56118fd89fdf9352fcdf03fd1bb50aaa61447
lgu38 <lgu38@DESKTOP-P5LDDTR>
2020-10-06 14:06:32 +0900

    온라인 계약 기초 함수 업데이트 20201006 14:05 이건욱 commit

--- src/main/java/com/drumtong/business/controller/contract/BusinessContractController.java
+++ src/main/java/com/drumtong/business/controller/contract/BusinessContractController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.drumtong.business.service.contract.BusinessContractService;
+import com.drumtong.business.vo.BInformationVO;
+import com.drumtong.business.vo.BManagementVO;
+import com.drumtong.business.vo.BMenuVO;
+import com.drumtong.business.vo.BPaymentVO;
 
 @Controller
 @RequestMapping("business/contract/")
@@ -16,14 +20,30 @@ public class BusinessContractController {
 	
 	// 비즈니스 온라인 계약 페이지로 이동 (GET) [건욱]
 	@RequestMapping(value = "businessContract/", method = RequestMethod.GET)
-		public ModelAndView contract() {
+	public ModelAndView contract() {
 		return svc.contract();
 	}	
 	
-	// 비즈니스 온라인 계약 페이지로 이동 (GET) [건욱]
+	// 온라인 계약 매장 생성 (POST) [건욱]  
+	// ※ BManagementVO의 DefayktCategory(메뉴 카테고리)는 Javascript에서 "/"를 넣어준다
+	// ex) 일반의류/신발/카페트/
+	@RequestMapping(value = "businessContract/", method = RequestMethod.POST)
+	public ModelAndView contract(BInformationVO bInformationVO, BPaymentVO bPaymentVO, BManagementVO bManagementVO) {
+		return svc.contract(bInformationVO, bPaymentVO, bManagementVO);
+	}
+	
+	
+	// 비즈니스 프리미엄 광고 계약 페이지로 이동 (GET) [건욱]
 	@RequestMapping(value = "businessPremiumAd/", method = RequestMethod.GET)
-		public ModelAndView premiumAd() {
+	public ModelAndView premiumAd() {
 		return svc.premiumAd();
 	}		
+	
+	// 비즈니스 프리미엄 광고 계약 (POST) [건욱]
+	@RequestMapping(value = "businessPremiumAd/", method = RequestMethod.POST)
+	public ModelAndView premiumAd(BInformationVO bInformationVO, BPaymentVO bPaymentVO) {
+		return svc.premiumAd(bInformationVO, bPaymentVO);
+	}
+	
 	
 }
--- src/main/java/com/drumtong/business/dao/BPaymentDAO.java
+++ src/main/java/com/drumtong/business/dao/BPaymentDAO.java
@@ -1,5 +1,14 @@
 package com.drumtong.business.dao;
 
+import com.drumtong.business.vo.BPaymentVO;
+
 public interface BPaymentDAO {
+
+	// 신규 데이터 생성
+	int insertSignUp(BPaymentVO bPaymentVO);
+
+
+	// 프리미엄 광고 결제수단 업데이트
+	int updatePremiumPay(BPaymentVO bPaymentVO);
 
 }
