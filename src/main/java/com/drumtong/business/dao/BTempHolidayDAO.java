1eaf4fbf7496b1f4cea33adcd4d6c76bb9df8f96
lgu38 <lgu38@DESKTOP-P5LDDTR>
2020-10-14 16:33:53 +0900

    이건욱 : 주요관리 백엔드 동작 작성 및 AWS S3 이미지 저장 20201014 16:33 commit

--- src/main/java/com/drumtong/business/dao/BTempHolidayDAO.java
+++ src/main/java/com/drumtong/business/dao/BTempHolidayDAO.java
@@ -1,5 +1,10 @@
 package com.drumtong.business.dao;
 
+import com.drumtong.business.vo.BTempHolidayVO;
+
 public interface BTempHolidayDAO {
+	
+	// 영업임시휴일관리 {임시휴일 시작일, 임시휴일 마지막일}을 업데이트 시켜준다.
+	int insertConstract(BTempHolidayVO bTempHolidayVO);
 
 }
