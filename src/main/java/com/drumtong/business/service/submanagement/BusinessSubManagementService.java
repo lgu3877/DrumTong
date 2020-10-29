a7aa34431bfaed28331856ea757fb53b6bf82f80
lgu38 <lgu38@DESKTOP-P5LDDTR>
2020-10-29 17:02:13 +0900

    이건욱 : 일정관리 및 프리미엄광고 백엔드 작업 20201029 17:02 commit

--- src/main/java/com/drumtong/business/service/submanagement/BusinessSubManagementService.java
+++ src/main/java/com/drumtong/business/service/submanagement/BusinessSubManagementService.java
@@ -1,44 +1,88 @@
 package com.drumtong.business.service.submanagement;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.drumtong.business.vo.BInformationVO;
 
 @Service
 public class BusinessSubManagementService {
 
 
 	// 비즈니스 리뷰관리 페이지로 이동 (GET) [건욱]
-	public ModelAndView reviewManagement() {
-		ModelAndView mav = new ModelAndView("business/submanagement/businessReviewManagement");
+	public ModelAndView reviewManagement(HttpServletRequest req) {
+
+		// Status 계약 여부 필드를 세션을 받아와준다.
+		String bol = ((BInformationVO)req.getSession().getAttribute("selectEST")).getStatus();
+
+		// boolean의 결과 값에 따라 'SUCCESS'이면 business로 우회해주고 'FAIL'이면 서브관리 페이지들로 이동시켜준다.
+		String route = bol.equals("SUCCESS") ? "redirect:/business/" : "business/submanagement/businessReviewManagement"; 
+
+		
+		ModelAndView mav = new ModelAndView(route);
 		return mav;
 	}
 
 
 	// 비즈니스 쿠폰관리 페이지로 이동 (GET) [건욱]
-	public ModelAndView couponManagement() {
-		ModelAndView mav = new ModelAndView("business/submanagement/businessCouponManagement");
+	public ModelAndView couponManagement(HttpServletRequest req) {
+		
+		// Status 계약 여부 필드를 세션을 받아와준다.
+		String bol = ((BInformationVO)req.getSession().getAttribute("selectEST")).getStatus();
+
+		// boolean의 결과 값에 따라 'SUCCESS'이면 business로 우회해주고 'FAIL'이면 서브관리 페이지들로 이동시켜준다.
+		String route = bol.equals("SUCCESS") ? "redirect:/business/" : "business/submanagement/businessReviewManagement"; 
+
+		
+		ModelAndView mav = new ModelAndView(route);
 		return mav;
 	}
 
 
 	// 비즈니스 카드계좌관리 페이지로 이동 (GET) [건욱]
-	public ModelAndView cardAccountManagement() {
-		ModelAndView mav = new ModelAndView("business/submanagement/businessCardAccountManagement");
+	public ModelAndView cardAccountManagement(HttpServletRequest req) {		// Status 계약 여부 필드를 세션을 받아와준다.
+
+		// Status 계약 여부 필드를 세션을 받아와준다.
+		String bol = ((BInformationVO)req.getSession().getAttribute("selectEST")).getStatus();
+
+		// boolean의 결과 값에 따라 'SUCCESS'이면 business로 우회해주고 'FAIL'이면 서브관리 페이지들로 이동시켜준다.
+		String route = bol.equals("SUCCESS") ? "redirect:/business/" : "business/submanagement/businessCardAccountManagement"; 
+
+		
+		ModelAndView mav = new ModelAndView(route);
 		return mav;
 	}
 
 
 
 	// 비즈니스 통계관리 페이지로 이동 (GET) [건욱]
-	public ModelAndView statisticsManagement() {
-		ModelAndView mav = new ModelAndView("business/submanagement/businessStatisticsManagement");
+	public ModelAndView statisticsManagement(HttpServletRequest req) {
+		
+		// Status 계약 여부 필드를 세션을 받아와준다.
+		String bol = ((BInformationVO)req.getSession().getAttribute("selectEST")).getStatus();
+
+		// boolean의 결과 값에 따라 'SUCCESS'이면 business로 우회해주고 'FAIL'이면 서브관리 페이지들로 이동시켜준다.
+		String route = bol.equals("SUCCESS") ? "redirect:/business/" : "business/submanagement/businessStatisticsManagement"; 
+
+		
+		ModelAndView mav = new ModelAndView(route);
 		return mav;
 	}
 
 
 	// 비즈니스 주문현황 관리 페이지로 이동 (GET) [건욱]
-	public ModelAndView orderStatusManagement() {
-		ModelAndView mav = new ModelAndView("business/submanagement/businessOrderStatusManagement");
+	public ModelAndView orderStatusManagement(HttpServletRequest req) {
+		
+		// Status 계약 여부 필드를 세션을 받아와준다.
+		String bol = ((BInformationVO)req.getSession().getAttribute("selectEST")).getStatus();
+
+		// boolean의 결과 값에 따라 'SUCCESS'이면 business로 우회해주고 'FAIL'이면 서브관리 페이지들로 이동시켜준다.
+		String route = bol.equals("SUCCESS") ? "redirect:/business/" : "business/submanagement/businessOrderStatusManagement"; 
+
+		
+		ModelAndView mav = new ModelAndView(route);
 		return mav;
 	}
 
