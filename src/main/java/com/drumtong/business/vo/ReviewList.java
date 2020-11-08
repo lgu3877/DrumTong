2247fc142e46370133bb79e45e1b365a9a42b9b6
dongsena <dongsena@naver.com>
2020-11-08 17:56:10 +0900

    이영경 : 리뷰리스트 클래스에 고객 프로필 사진 주소 필드 추가 	리뷰에 고객 이름 모자이크 -> EX) 이영경 -> 이영*** 	breviewvo에 gpa 필드 int-> double로 수정 20201108 17:56 commit

--- src/main/java/com/drumtong/business/vo/ReviewList.java
+++ src/main/java/com/drumtong/business/vo/ReviewList.java
@@ -4,20 +4,28 @@ public class ReviewList extends BReviewVO{
 //	private String memberid;
 //	private String estid;
 //	private String salecode;
-//	private int gpa;
-//	private int mgood;
-//	private int reportcount;
+//	private double gpa;			// 평점
+//	private int mgood;			// 좋아요
+//	private int reportcount;	// 신고횟수
 //	private char secreatbolean;
 //	private char deleteboolean;
 //	private String deletedate;
-//	private String reviewimg;
-//	private char replyboolean;
-//	private char blockboolean;
+//	private String reviewimg;	// 리뷰 사진	
+//	private char replyboolean;	// 사장님 답글 여부
+//	private char blockboolean;	// 사장님 신고 여부
 	private String ccontent;	// 고객 리뷰
 	private String cregistdate;	// 고객 리뷰 생성 날짜
 	private String bcontent;	// 사업자 답글
 	private String bregistdate;	// 사업자 답글 날짜
-	private String customerName;	// 고객 이름
+	private String customerName;	// 고객 이름(앞에 두 글자 나오고, 무조건 별 3개 붙이도록 설정 EX 이름: 이영경 -> 이영***)
+	private String profileimg;		// 고객 프로필 사진 주소(없으면 null값이 들어가 있음)
+	
+	public String getProfileimg() {
+		return profileimg;
+	}
+	public void setProfileimg(String profileimg) {
+		this.profileimg = profileimg;
+	}
 	public String getCcontent() {
 		return ccontent;
 	}
