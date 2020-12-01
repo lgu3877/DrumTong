1a464900c3ac60c078cc07828f1ba57a2988c00d
Euijin Yang <trustnloveu@gmail.com>
2020-12-01 17:01:56 +0900

    양의진, 매장 관리 상품 리스트 수정 폼 구현 & 버튼 클릭 에러 제거
    20201201 17:01

--- src/main/webapp/resources/business/js/shopmanagement/businessShopManagement.js
+++ src/main/webapp/resources/business/js/shopmanagement/businessShopManagement.js
@@ -144,7 +144,7 @@ function comfirmIntro() {
 function directType(obj) {
 	console.log('this : ', obj);
 	console.log('this : ', obj.value);
-	if(obj.value == 'selectedDirect') {
+	if(obj.value === 'selectedDirect') {
 		obj.parentNode.querySelector('.direct_type_input').style.display = '';
 	}
 	else {
@@ -162,7 +162,10 @@ const updateItemBtn = document.getElementById("update-item-btn");
 const itemlist = document.getElementById('add-item-list')
 // 입력 줄 전체
 let singleList = document.querySelector('.single_item_selector');
-
+// 메뉴 테이블 수정 버튼
+const modifyListBtn = document.getElementById("update-list-btn");
+// 테이블 내부 행 삭제 버튼
+const deleteBtn = document.getElementsByClassName("delete_item");
 
 
 // 취소 - 클릭 이벤트(1줄씩 추가)
@@ -177,6 +180,171 @@ updateItemBtn.addEventListener('click', function() {
 	console.log('Update List');
 })
 
+// 메뉴 수정 버튼(th & td > input + 삭제 버튼)
+modifyListBtn.addEventListener('click', function() {
+	
+	// 삭제 활성화가 되어 있을 시(폼 변경 후), 작동안함
+	if(document.getElementById("delete_title")) {
+		return;
+	}
+	
+	const tableHead = document.getElementsByClassName("current_menu_table")[1].getElementsByTagName("thead")[0];
+	const tableBody = document.getElementsByClassName("current_menu_table")[1].getElementsByTagName("tbody")[0];
+	
+	// 상품 리스트 수(= 행 개수)
+	const tr = tableBody.getElementsByTagName("tr");
+	const trLength = tr.length;
+	
+	// 열 길이
+	const columnLength = tableHead.children[0].children.length;
+	
+	// 수정 폼 타입
+	// const types = [ "first_select", "second_select", "detail", "price", "time"];
+	
+	// 테이블(thead) 삭제 열 추가
+	const deleteList = document.createElement("th");
+	deleteList.setAttribute("id", "delete_title");
+	deleteList.scope = "cols";
+	deleteList.innerHTML = "삭제";
+	tableHead.children[0].appendChild(deleteList);
+	
+	// 테이블(tbody) 삭제 버튼 추가 & 폼(input) 변경
+	for(let i = 0; i < tr.length; i++) {
+		const td = document.createElement("td");
+		
+		// 내부 폼 변경
+		for (let j = 0; j < columnLength; j++) {
+			const select = document.createElement("select");
+			const defaultOption = document.createElement("option");
+			const testOption = document.createElement("option");
+			const typeOption = document.createElement("option");
+			const directInput = document.createElement("input");
+
+			switch(j) {
+			// 첫번째 select
+			case 0:	
+				// 초기화
+				tr[i].children[j].innerHTML = "";
+
+				select.className = "service_selector";
+				select.name = ""
+				select.setAttribute("onchange", "directType(this)");
+				
+				// (DB) 옵션 추가(types ~ 직접입력)
+				// placeholder
+				defaultOption.hidden = "true";
+				defaultOption.disabled = "true";
+				defaultOption.selected = "true";
+				defaultOption.innerHTML = "초기값(DB)";			
+				defaultOption.value = "초기값(DB)";			
+				
+				// 기타 옵션들(사용자에 따라 설정된 옵션)
+				testOption.innerHTML = "Service 1";
+				
+				// 직접입력
+				typeOption.value = "selectedDirect";
+				typeOption.className = "selectedDirect"; 
+				typeOption.innerHTML = "직접입력";
+				
+				// 옵션 주입
+				select.appendChild(defaultOption);
+				select.appendChild(testOption);
+				select.appendChild(typeOption);
+				
+				// 직접입력 input 생성
+				directInput.type="text";
+				directInput.className = "direct_type_input";
+				directInput.name = "";
+				directInput.placeholder = "서비스 입력";
+				directInput.style.display = "none";
+				
+				// select & input 주입
+				tr[i].children[j].appendChild(select);
+				tr[i].children[j].appendChild(directInput);
+				
+				break;
+			// 두번째 select
+			case 1:
+				// 초기화
+				tr[i].children[j].innerHTML = "";
+
+				select.className = "service_selector";
+				select.name = ""
+				select.setAttribute("onchange", "directType(this)");
+				
+				// (DB) 옵션 추가(types ~ 직접입력)
+				// placeholder
+				defaultOption.hidden = "true";
+				defaultOption.disabled = "true";
+				defaultOption.selected = "true";
+				defaultOption.innerHTML = "초기값(DB)";			
+				defaultOption.value = "초기값(DB)";			
+				
+				// 기타 옵션들(사용자에 따라 설정된 옵션)
+				testOption.innerHTML = "Type 1";
+				
+				// 직접입력
+				typeOption.value = "selectedDirect";
+				typeOption.className = "selectedDirect"; 
+				typeOption.innerHTML = "직접입력";
+				
+				// 옵션 주입
+				select.appendChild(defaultOption);
+				select.appendChild(testOption);
+				select.appendChild(typeOption);
+				
+				// 직접입력 input 생성
+				directInput.type="text";
+				directInput.className = "direct_type_input";
+				directInput.name = "";
+				directInput.placeholder = "서비스 입력";
+				directInput.style.display = "none";
+				
+				// select & input 주입
+				tr[i].children[j].appendChild(select);
+				tr[i].children[j].appendChild(directInput);
+				
+				break;
+			// 세부내용
+			case 2:	
+				break;
+			// 가격
+			case 3:	
+				break;
+			// 시간
+			case 4:	
+				break;
+			}
+			let value = tr[i].children[j].innerHTML;
+//			console.log(value);
+			
+		}
+		
+		// 홀수 행(흰색 배경)
+		if(i % 2 !== 0) {
+			
+			
+			td.setAttribute("class", "even delete_item");
+			td.innerHTML = "<div onclick='deleteList(this.parentElement.parentElement)'>" + "<i class='fas fa-times'></i>" + "<span>삭제</span>" + "</div>";
+		}
+		// 짝수 행(하늘색 배경)
+		else {
+			td.setAttribute("class", "delete_item");
+			td.innerHTML = "<div onclick='deleteList(this.parentElement.parentElement)'>" + "<i class='fas fa-times'></i>" + "<span>삭제</span>" + "</div>";			
+		}
+		
+		tr[i].appendChild(td);						
+		
+	}
+});
+
+// 상품 리스트 삭제
+function deleteList(tr) {
+	const answer = confirm("해당 목록을 삭제하시겠습니까?");
+	if (answer === true ) {
+		tr.remove();		
+	}
+}
 
 // 취소 버튼
 function dismissInput(entireNode) {
