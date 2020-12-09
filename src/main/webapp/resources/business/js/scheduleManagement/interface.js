1218bcc2316bd02304b45840eea31b1f7a092f64
Euijin Yang <trustnloveu@gmail.com>
2020-12-09 17:45:21 +0900

    양의진, radio > checkbox 연동 문제 해결. CSS 추가 작업
    20201209 17:45

--- src/main/webapp/resources/business/js/scheduleManagement/interface.js
+++ src/main/webapp/resources/business/js/scheduleManagement/interface.js
@@ -1,3 +1,4 @@
+// checkbox 체크 옵션
 function checkOption(id) {
 	const checked = document.getElementById(id).checked;
 	checked === false ? 
@@ -6,25 +7,35 @@ function checkOption(id) {
 	
 }
 
+
+// radio 체크 옵션
 function radioSelect(id) {
 	document.getElementById(id).checked = true;
 }
 
-function filterCheckbox(checkId, targetClass) {
-	const filter = document.getElementById(checkId).checked;
-	const targets = document.getElementsByClassName(targetClass);
-	
-	console.log(filter);
-	console.log(targets);
-	
-	if (filter === "checked") {
-		for (let i = 0; i < targets.length; i++) {
-			targets[i].removeAttribute("disabled");
+
+// radio 버튼 이벤트 생성	>	checkbox disabled 속성 변경
+const radioBtns = document.getElementsByClassName("radioBtn");
+
+for (let i = 0; i < radioBtns.length; i++) {
+	radioBtns[i].addEventListener('click', () => {
+		
+		const filter = document.getElementById('weekend-also').checked;
+		const checkboxs = document.getElementsByClassName("checkBtn");
+		
+		console.log(filter);
+		console.log(checkboxs);
+		
+		if (filter === true) {
+			for (let i = 0; i < checkboxs.length; i++) {
+				checkboxs[i].removeAttribute("disabled");
+			}
+		} 
+		else {
+			for (let i = 0; i < checkboxs.length; i++) {
+				checkboxs[i].setAttribute("disabled", "disabled");
+				console.log("123")
+			}		
 		}
-	} 
-	else {
-		for (let i = 0; i < targets.length; i++) {
-			targets[i].setAttribute("disabled", "disabled");
-		}		
-	}
+	});
 }
