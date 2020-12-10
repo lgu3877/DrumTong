00423e6890932c2732ba376f68bc0c59d5dbfc46
lgu38 <lgu38@DESKTOP-P5LDDTR>
2020-12-10 17:10:40 +0900

    이건욱 : 계좌 / 카드 REST Update 20201210 17:10 commit

--- src/main/java/com/drumtong/business/dao/BPaymentDAO.java
+++ src/main/java/com/drumtong/business/dao/BPaymentDAO.java
@@ -8,10 +8,6 @@ public interface BPaymentDAO {
 	int insertSignUp(BPaymentVO bPaymentVO);
 
 
-	// 프리미엄 광고 결제수단 업데이트 / REST일 시에도 사용
-	int updatePremiumPay(BPaymentVO bPaymentVO);
-
-
 	// 통장사본을 업데이트 시켜줍니다. S3에 사용
 	int updateCopyOfBankBook(BPaymentVO vo);
 
@@ -21,6 +17,15 @@ public interface BPaymentDAO {
 
 	// Bpayment 테이블 데이터를 전부 불러옵니다.
 	BPaymentVO selectList(String estid);
+
+	
+//================REST=============================
+	
+	// 프리미엄 광고 결제수단 업데이트 / REST일 시에도 사용
+	int updatePremiumPay(BPaymentVO bPaymentVO);
+
+	// 계좌 업데이트 (REST)
+	int updateAccount(BPaymentVO bPaymentVO);
 
 
 
