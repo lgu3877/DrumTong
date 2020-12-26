3176fa9571a2dab7358e63cb76c92df3fb43ea12
YoungKyeongLee <dongsena@naver.com>
2020-12-26 17:36:21 +0900

    이영경 : 메인 랜덤 리스트 출력 백엔드 작업은 완료 20201226 17:35 commit

--- src/main/java/com/drumtong/customer/service/main/CustomerMainService.java
+++ src/main/java/com/drumtong/customer/service/main/CustomerMainService.java
@@ -58,7 +58,6 @@ public class CustomerMainService {
 	
 	// 랜덤으로 10개 매장을 들고 오는 메서드[영경]
 	private List<EstablishmentList> randomList(HttpServletRequest req){
-		System.out.println("randomList 메서드 실행");
 		CPrivateDataVO Login = (CPrivateDataVO)req.getSession().getAttribute("cLogin");
 		boolean isUser = Login != null;
 		// 로그인 했을 땐 수령지 주소 기준(메인 주소만)으로 안했을 땐 기본 주소로
@@ -111,28 +110,19 @@ public class CustomerMainService {
 		int maxNum = list.size();
 		boolean checkList = true;
 		
-//		System.out.println("■■■ 리스트 테스트 ■■■");
-//		for(EstablishmentList rlist : list) {
-//			System.out.println(rlist.getEstid() + " : " + rlist.getBrandnaming() + ", " + rlist.getGpa());
-//		}
-//		System.out.println("■■■ size : " + result.size() + "개 ■■■");
-		
-		//1. 처음 리스트
+		//1. list 와 set을 중복없이 합친다.
 		if(maxNum < 10) {
 			if((result.size() + maxNum) <= 10) {
-				// 두 리스트 합치기■■■■■■■■■■■■■■■■■■■■■■■
-				
-				// 중복된 값은 지우고(removeAll : 차집합)
-				list.removeAll(result);
-				
-//				리스트 추가해주기(addAll : 합집합)
-				result.addAll(list);
-				
-//				System.out.println("■■■ 랜덤 리스트 테스트 ■■■");
-//				for(EstablishmentList rlist : result) {
-//					System.out.println(rlist.getEstid() + " : " + rlist.getBrandnaming() + ", " + rlist.getGpa());
-//				}
-//				System.out.println("■■■ size : " + result.size() + "개 ■■■");
+				for(EstablishmentList li : list) {
+					boolean Exist = true;
+					for(EstablishmentList re : result) {
+						if(li.getEstid().equals(re.getEstid()) ) {
+							Exist = !Exist;
+							break;
+						}
+					}
+					if(Exist) result.add(li);
+				}
 				
 				return result;
 			}
@@ -149,12 +139,6 @@ public class CustomerMainService {
 				}
 			}
 		}
-		
-//		System.out.println("■■■ 랜덤 리스트 테스트 ■■■");
-//		for(EstablishmentList rlist : result) {
-//			System.out.println(rlist.getEstid() + " : " + rlist.getBrandnaming() + ", " + rlist.getGpa());
-//		}
-//		System.out.println("■■■ size : " + result.size() + "개 ■■■");
 		
 		return result;
 	}
