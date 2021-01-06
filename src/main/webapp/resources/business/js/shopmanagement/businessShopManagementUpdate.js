a82d5f1e0c29e1e0c451a93cc1d47b0622eef63d
Euijin Yang <trustnloveu@gmail.com>
2021-01-06 13:30:40 +0900

    양의진, 퀵 서비스 post 작업 & 함수명 변경 & 서비스 등록 진행
    20210106 13:29

--- src/main/webapp/resources/business/js/shopmanagement/businessShopManagementUpdate.js
+++ src/main/webapp/resources/business/js/shopmanagement/businessShopManagementUpdate.js
@@ -8,7 +8,7 @@ async function updatePhoto() {
 	
 	for (let i = 0; i < photoInputs.length; i++) {
 		if (photoInputs[i].files[0] !== undefined) {
-			fileList.push(formDate.append("storeimg", photoInputs[i].files[0]));
+			fileList.push(formData.append("storeimg", photoInputs[i].files[0]));
 //			fileList.push(photoInputs[i].files[0]);
 		}
 	}	
@@ -19,7 +19,7 @@ async function updatePhoto() {
 //	};
 	
 	const object = {
-		"mainImage" : formDate.append("delegatephotoboolean", coverImage.files[0]),
+		"mainImage" : formData.append("delegatephotoboolean", coverImage.files[0]),
 		"subImage" : fileList,
 	};
 	
@@ -53,18 +53,53 @@ function addService() {
 	const object = [];
 	const classPrefix = ["first", "second", "third", "forth", "fifth"]
 	
+	// 상품 등록 횟수
 	for (let i = 0; i < list.length; i++) {
-		// select (2) & input (3)
-		const eachInput = list[i].getElementsByClassName(classPrefix[i] + '_item_prop')[0];
-		console.log(eachInput);
-		for (let j = 0; j < eachInput.length; j++) {
-			const element = {
+		let element = {}; // 상품 등록 객체
+		// 상품 등록 속성 5개 > 객체 생성 (대분류, 중분류, 세부내용, 가격, 시간) + 배달
+		for (let j = 0; j < classPrefix.length; j++) {
+			const eachInput = list[i].getElementsByClassName(classPrefix[j] + '_item_prop')[0];
+			// select (2) & input (3) || direct (2)
+			console.log(eachInput);
+			
+			// select & direct input
+			if (j === 0 || j === 1) {
+				const select = eachInput.getElementsByTagName("select")[0];
+				const options = eachInput.getElementsByClassName(name)
+				const direct = eachInput.getElementsByTagName("input")[0];
 				
-			};
+//				for (let z = = 0; z < select.getele)
+				
+				console.log(select);
+				console.log(direct);
+				
+				element[select.name] = "type";
+			}
+			
+			// text input
+			else {
+				
+			}
+			
 		}
+		object.push(element);
+		
+		console.log(element);
+		console.log(object);
 	}
 }
 
+// 배달 서비스 활성화 < 서비스 등록
+async function activateDelivery() {
+	const result= deliveryToggle  ? "y" : "n"; // js > MenuList
+
+	console.log(option);
+	console.log(result);
+
+	const { data } = await axios.post("/drumtong/business/mainmanagement/businessShopManagement/", result);
+	
+	console.log(data);
+}
 
 // 배달 업데이트
 async function updateDelivery() {
