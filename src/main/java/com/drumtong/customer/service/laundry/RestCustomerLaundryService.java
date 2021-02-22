40a0913fefcf2b3648b9f6e6c79fa1adb8ec43ba
YoungKyeongLee <dongsena@naver.com>
2021-02-22 14:57:19 +0900

    이영경 : 세탁소 검색 페이지 수정, page 관련 코드들 추가 20210222 14:57 commit

--- src/main/java/com/drumtong/customer/service/laundry/RestCustomerLaundryService.java
+++ src/main/java/com/drumtong/customer/service/laundry/RestCustomerLaundryService.java
@@ -113,36 +113,25 @@ public class RestCustomerLaundryService {
 	}
 
 	// 좌표 값 안에 해당되는 세탁소들을 들고오는 메서드[영경] 여기서부터 작업하기! 위도 경도 배치 정확하게 하기! 큰 위도, 작은 위도 등..
-	public String selectLaundry(String left, String right, String top, String bottom, String filter1, String filter2, String filter3, String filter4, String filter5) {
-		HashMap<String, String> param = new HashMap<String, String>();
-		List<Object> list = new ArrayList<Object>();
+	public String selectLaundry(String left, String right, String top, String bottom, HashMap<String, String> param) {
 		param.put("left", left);
 		param.put("right", right);
 		param.put("top", top);
 		param.put("bottom", bottom);
-		param.put("filter1", filter1);
-		param.put("filter2", filter2);
-		param.put("filter3", filter3);
-		param.put("filter4", filter4);
 		
-		param.put("premium", "Y");
-		list.add(bInformationDAO.selectEstablishmentList(param));
-		
-		param.put("premium", "N");
-		list.add(bInformationDAO.selectEstablishmentList(param));
-		return new Gson().toJson(list);
+		return getLaundryList(param);
 	}
 
 	// emd 코드가 일치하는 세탁소들을 들고오는 메서드[영경]
-	public String selectLaundry(String emdcode, String filter1, String filter2, String filter3, String filter4, String filter5) {
-		HashMap<String, String> param = new HashMap<String, String>();
-		List<Object> list = new ArrayList<Object>();
+	public String selectLaundry(String emdcode, HashMap<String, String> param) {
 		param.put("emdcode", emdcode);
-		param.put("filter1", filter1);
-		param.put("filter2", filter2);
-		param.put("filter3", filter3);
-		param.put("filter4", filter4);
 		
+		return getLaundryList(param);
+	}
+	
+	// 세탁소 리스트를 sql문 통해 적절히 가져와주는 내부 메서드
+	private String getLaundryList(HashMap<String, String> param) {
+		List<Object> list = new ArrayList<Object>();
 		param.put("premium", "Y");
 		list.add(bInformationDAO.selectEstablishmentList(param));
 		
@@ -151,5 +140,17 @@ public class RestCustomerLaundryService {
 		return new Gson().toJson(list);
 	}
 
+	// 매개변수들을 모아 hash map에 풋 해주는 함수
+	public HashMap<String, String> paramSet(String filter1, String filter2, String filter3, String filter4, String filter5, String page){
+		HashMap<String, String> param = new HashMap<String, String>();
+		param.put("filter1", filter1);
+		param.put("filter2", filter2);
+		param.put("filter3", filter3);
+		param.put("filter4", filter4);
+		param.put("filter5", filter5);
+		param.put("page", page);
+		return param;
+	}
+	
 
 }
