c399e2e018920daf227379bda34349c0cc2e0d3e
Euijin Yang <trustnloveu@gmail.com>
2021-02-26 14:34:28 +0900

    양의진, 서비스 소개 > 슬라이드 테스트
    20210226 14:34

--- src/main/webapp/resources/business/js/intropage.js
+++ src/main/webapp/resources/business/js/intropage.js
@@ -1,18 +1,31 @@
+// viewer container
+const container = document.getElementById("viewer-container");
+
+const initializer = document.getElementsByClassName("viewer").length * 100;
 
 
-const wrapper = document.getElementById("viewer-wrapper");
 
-wrapper.addEventListener("mousewheel", function(e) {
-	console.log(e.wheelDelta);
-
+// scroll event
+container.onmousewheel = (e) => {
 	const wheel = e.wheelDelta; // scroll movement value
+//	console.log(wheel);
+	
+	const execution = 100;
 	
 	// wheel up
-	if (wheel > 0)
+	if (wheel > 0) {
 		console.log("scroll up");
 		
+		
+		container.style.transform = "translateY(" + execution + "vh)";
+	}
+
 	// wheel down
 	else {
+		
 		console.log("scroll down");
+
+		container.style.transform = "translateY(-" + execution + "vh)";
+//		container.style.transform = "translateY(-" + indicator + "vh)";
 	}
-});
+};
