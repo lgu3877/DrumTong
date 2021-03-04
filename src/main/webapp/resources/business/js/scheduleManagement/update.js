07ba86701392355e591eb51d2eaba5d946770c4f
Euijin Yang <trustnloveu@gmail.com>
2021-03-04 16:21:31 +0900

    양의진, 일정관리 > 정기휴무 > axios request 포멧 수정 완료
    20210304 16:21

--- src/main/webapp/resources/business/js/scheduleManagement/update.js
+++ src/main/webapp/resources/business/js/scheduleManagement/update.js
@@ -13,105 +13,27 @@ function removeRepeatValue(array) {
 
 // 정기 휴무 등록
 async function updateRegHoliday() {
-	const initial = {};
-	const updated = {};
-	const concat = {};
-	const copiedConcat = {};
-	
-	// 초기 객체
-	for (let week in initialRegHoliday) {
-		const dayArray = initialRegHoliday[week].split("/");
-		dayArray.pop();
-		
-		initial[week] = dayArray;
-	}
-
-	// 업데이트된 객체
-	for (let week in bscheduledays) {
-		const dayArray = bscheduledays[week].split("/");
-		dayArray.pop();
-		
-		updated[week] = dayArray;
-	}
-	
-	// 객체 합(초기 + 업데이트)
-	const concatweeks = removeRepeatValue([ ...Object.keys(initial), ...Object.keys(updated) ]);
-	
-	for (let i = 0; i < concatweeks.length; i++) {
-		const week = concatweeks[i];
-		
-		const initialDays = initial[week] || [];
-		const updatedDays = updated[week] || [];
-		
-		copiedConcat[week] = removeRepeatValue([ ...initialDays, ...updatedDays ]);
-		concat[week] = removeRepeatValue([ ...initialDays, ...updatedDays ]);
-	}
-	
-	// request 생성
-	const object = {
-		"add" : {},
-		"remove" : {}
-	}
-	
-	// 객체 업데이트(add & remove values)
-	// add = concat - initial
-	for (let week in concat) {
-		if (initial[week] === undefined) {
-			object.add[week] = concat[week]; // 추가
-		}
-		
-		else {
-			for (let i = 0; i < initial[week].length; i++) {
-				const day = initial[week][i];
-				const index = concat[week].indexOf(day)
-				concat[week].splice(index, 1);
-			}
-			if (concat[week].length !== 0) { // 추가
-				object.add[week] = concat[week];
-			} 
-		}
-	}
-	
-	// remove = concat - updated
-	for (let week in copiedConcat) {
-		if (updated[week] === undefined) {
-			object.remove[week] = copiedConcat[week]; // 추가
-		}
-		
-		else {
-			for (let i = 0; i < updated[week].length; i++) {
-				const day = updated[week][i];
-				const index = copiedConcat[week].indexOf(day)
-				copiedConcat[week].splice(index, 1);
-			}
-			if (copiedConcat[week].length !== 0) { 
-				object.remove[week] = copiedConcat[week]; // 추가
-			} 
-		}
-	}	
-	
-	console.log(object);
-
 	// axios
-//	const { data } = await axios.post("/drumtong/business/mainmanagement/BScheduleDays/rest/insertBScheduleDays/", object);
-//	console.log(data);
-//
-//	if (data) {
-//		bscheduledays = data; // 데이터 덮어씌우기
-//		document.getElementById("reg-holiday-schedule").innerHTML = ""; // 정기휴무 리스트 초기화
-//		displayRegHolidays(); // 정기휴무 리스트 다시 출력
-//	}
-//	
-//	else {
-//		alert("잘못된 입력입니다.");
-//	}
+	const { data } = await axios.post("/drumtong/business/mainmanagement/BScheduleDays/rest/insertBScheduleDays/", bscheduledays);
+
+	if (data) {
+		bscheduledays = data; // 데이터 덮어씌우기
+		document.getElementById("reg-holiday-schedule").innerHTML = ""; // 정기휴무 리스트 초기화
+
+		// 정기휴무 뷰 업데이트
+		displayRegHolidays(); // 정기휴무 리스트 다시 출력
+
+		// selct & checkbox 초기화
+		
+		
+		// 캘린더 업데이트
+	}
+	
+	else {
+		alert("잘못된 입력입니다.");
+	}
 	
 	
-	// selct & checkbox 초기화
-	
-	// 정기휴무 뷰 업데이트
-	
-	// 캘린더 업데이트
 	
 }
 
